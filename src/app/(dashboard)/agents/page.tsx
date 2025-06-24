501121e8767eeb4da89dab0ae87b08f198b428aa
shivenaggarwal <shivenagg04@gmail.com>
2025-06-24 16:57:18 +0530

    agent setup

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,7 +1,26 @@
-import { AgentsView } from "@/modules/agents/ui/view/agents-view";
+import {
+  AgentsView,
+  AgentsViewLoading,
+  AgentViewError,
+} from "@/modules/agents/ui/view/agents-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
 
-const Page = () => {
-  return <AgentsView />;
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentsViewLoading />}>
+        <ErrorBoundary fallback={<AgentViewError />}>
+          <AgentsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -1,14 +1,14 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, baseProcedure } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
+// import { TRPCError } from "@trpc/server";
 
 export const agentRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    throw new TRPCError({ code: "BAD_REQUEST" });
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    // throw new TRPCError({ code: "BAD_REQUEST" });
 
     return data;
   }),
