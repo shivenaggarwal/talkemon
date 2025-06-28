bef9f6186d5d4af9fc6a1a42295134f9900c3f3c
shivenaggarwal <shivenagg04@gmail.com>
2025-06-28 20:13:47 +0530

    last fix hopefully

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,10 +1,16 @@
+import { GeneratedAvatar } from "@/components/generated-avatar";
 import {
   CommandResponsiveDialog,
   CommandInput,
   CommandItem,
   CommandList,
+  CommandGroup,
+  CommandEmpty,
 } from "@/components/ui/command";
-import { Dispatch, SetStateAction } from "react";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useState } from "react";
 
 interface Props {
   open: boolean;
@@ -12,11 +18,76 @@ interface Props {
 }
 
 export const DashboardCommand = ({ open, setOpen }: Props) => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const trpc = useTRPC();
+
+  const meetings = useQuery(
+    trpc.meetings.getMany.queryOptions({
+      search,
+      pageSize: 100,
+    })
+  );
+
+  const agents = useQuery(
+    trpc.agents.getMany.queryOptions({
+      search,
+      pageSize: 100,
+    })
+  );
+
   return (
-    <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
-      <CommandInput placeholder="find a meeting or agent." />
+    <CommandResponsiveDialog
+      shouldFilter={false}
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <CommandInput
+        placeholder="find a meeting or agent..."
+        value={search}
+        onValueChange={(value) => setSearch(value)}
+      />
       <CommandList>
-        <CommandItem>test</CommandItem>
+        <CommandGroup heading="Meetings">
+          <CommandEmpty>
+            <span className="text-muted-foreground text-sm">
+              No meetings found
+            </span>
+          </CommandEmpty>
+          {meetings.data?.items.map((meeting) => (
+            <CommandItem
+              key={meeting.id}
+              onSelect={() => {
+                router.push(`/meetings/${meeting.id}`);
+              }}
+            >
+              {meeting.name}
+            </CommandItem>
+          ))}
+        </CommandGroup>
+        <CommandGroup heading="Agents">
+          <CommandEmpty>
+            <span className="text-muted-foreground text-sm">
+              No agents found
+            </span>
+          </CommandEmpty>
+          {agents.data?.items.map((agent) => (
+            <CommandItem
+              key={agent.id}
+              onSelect={() => {
+                router.push(`/agents/${agent.id}`);
+              }}
+            >
+              <GeneratedAvatar
+                seed={agent.name}
+                variant="botttsNeutral"
+                className="size-5"
+              />
+              {agent.name}
+            </CommandItem>
+          ))}
+        </CommandGroup>
       </CommandList>
     </CommandResponsiveDialog>
   );
